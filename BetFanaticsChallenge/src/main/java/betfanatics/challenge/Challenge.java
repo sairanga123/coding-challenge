31665c3e345e8ac67d2f9462a7141dfed35d716e
sairanga123 <saiprasanna100@gmail.com>
2022-08-26 18:50:50 -0700

    Bet Fanatics Coding Challenge

---
+++ BetFanaticsChallenge/src/main/java/betfanatics/challenge/Challenge.java
@@ -0,0 +1,163 @@
+package betfanatics.challenge;
+
+import okhttp3.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Challenge {
+    final String ACCESS_KEY = "ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e";
+
+    public Challenge() {
+    }
+
+    /**
+     * 1-3) get sorted users from page three and logs out the total number of pages
+     * @param
+     * @return List<JSONObject> list of users sorted
+     */
+    public List<JSONObject> getSortedUsersFromPageThree() throws Exception {
+        // using arraylist to store the user list that we are getting back
+        List<JSONObject> userList = new ArrayList<JSONObject>();
+
+        try {
+
+            // use HTTP Url connection in order to make get call and get JSON Object
+            // 1) Make the request to get page 3 of the users (?page=3) in the url
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url("https://gorest.co.in/public/v2/users?access-token=" + ACCESS_KEY + "&page=3")
+                    .method("GET", null)
+                    .addHeader("X-Pagination-Pages", "")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+            JSONParser parser = new JSONParser();
+
+            // 1) Result for get the users from Page 3
+            JSONArray usersFromPageThree = (JSONArray) parser.parse(data);
+
+            // 2) print out the pagination total - total number of pages from the previous request
+            int paginationTotal = Integer.parseInt(response.headers().get("x-pagination-pages").toString());
+            System.out.println(paginationTotal);
+
+            //3) Construct a data structure to hold users and sort them -> return the sorted list of users
+            for(int i = 0; i < usersFromPageThree.size(); i++) {
+                JSONObject user = (JSONObject) usersFromPageThree.get(i);
+                userList.add(user);
+            }
+
+            Collections.sort(userList, new Comparator<JSONObject>() {
+                @Override
+                public int compare(JSONObject o1, JSONObject o2) {
+                    return o1.get("name").toString().compareTo(o2.get("name").toString());
+                }
+            });
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return userList;
+    }
+
+    /**
+     * 4) get the name of the last user
+     * @param  users - List<JSONObjects>
+     * @return String
+     */
+    public String getLastUserFromList(List<JSONObject> users) {
+        return users.get(users.size()-1).get("name").toString();
+    }
+
+    /**
+     * 5) update last user's name to new value and save it using a patch call
+     * @param  users - List<JSONObjects>
+     * @return JSONObject
+     */
+    public JSONObject updateLastUserNameAndSave(List<JSONObject> users) {
+        JSONObject changedNameObj = null;
+        int lastUserId = Integer.parseInt(users.get(users.size()-1).get("id").toString());
+        System.out.println(lastUserId);
+        String urlBuilder = "https://gorest.co.in/public/v2/users/" + lastUserId + "?access-token=" + ACCESS_KEY;
+
+        // Set new name of user and save it as Peter Parker for that user id
+        String jsonPayload = " {\n        \"name\": \"Peter Parker\"\n\n}";
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, jsonPayload);
+            Request request = new Request.Builder()
+                    .url(urlBuilder)
+                    .method("PATCH", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            Response response = client.newCall(request).execute();
+            JSONParser parser = new JSONParser();
+            changedNameObj = (JSONObject) parser.parse(response.body().string());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return changedNameObj;
+    }
+
+    /**
+     * 6) Delete the user whose name was changed last - takes in an object to delete and
+     *    calls the delete method
+     * @param objectToDelete JSONObject
+     * @return void
+     */
+    public void deleteUserByUserObject(JSONObject objectToDelete) {
+        try {
+            String deleteUrl = "https://gorest.co.in/public/v2/users/" + objectToDelete.get("id") +
+                    "?access-token=" + ACCESS_KEY;
+
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody body = RequestBody.create(mediaType, "");
+            Request request = new Request.Builder()
+                    .url(deleteUrl)
+                    .method("DELETE", body)
+                    .build();
+            Response response = client.newCall(request).execute();
+
+            if(response.isSuccessful()) {
+                System.out.println("User " + objectToDelete.get("id") + " has been successfully deleted");
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 7) Request for NonExistent user - gets the response code for a user id
+     * @param userId int
+     * @return int responseCode
+     */
+    public int getResponseCodeById(String userId) {
+        String requestUrl = "https://gorest.co.in/public/v2/users/" + userId +
+                "?access-token=" + ACCESS_KEY;
+
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url(requestUrl)
+                    .method("GET", null)
+                    .addHeader("X-Pagination-Pages", "")
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.code();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+}
---
+++ BetFanaticsChallenge/src/test/java/betfanatics/challenge/ChallengeTest.java
@@ -0,0 +1,224 @@
+package betfanatics.challenge;
+
+import junit.framework.TestCase;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.IOException;
+import java.util.*;
+
+/*
+
+This is the testing file for the Challenge Class, it should be able to call the methods from the Challenge
+and execute the outputs accordingly.
+
+1-3. get sorted users from page three
+List<JSONObject> users = getSortedUsersFromPageThree();
+
+4. get the name of the last user
+String lastUser = getLastUserFromList(users);
+
+5. update last user's name to new value and save it
+JSONObject changeNameObject = updateLastUserNameAndSave(users);
+
+6. Delete the user whose name was changed last
+deleteUserByUserObject(changeNameObject);
+
+7. Request for NonExistent user
+int responseStatusFromCall = getUserById("5555");
+
+ */
+
+public class ChallengeTest extends TestCase {
+
+    @Test
+    @DisplayName("Test to get Sorted Users from Page 3 and print total # of pages")
+    public void testGetSortedUsersFromPageThree() {
+        Challenge challenge = new Challenge();
+        List<JSONObject> userList = null;
+        try {
+            userList  = challenge.getSortedUsersFromPageThree();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // use private method to mock API call and see if the two lists are matching
+        String url = "https://gorest.co.in/public/v2/users?access-token=ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e&page=3";
+        List<JSONObject> test = getUsersFromUrl(url);
+
+        // check if both lists are matching and check if both numbers printed out are the same
+        Assert.assertEquals(userList, test);
+    }
+
+    @Test
+    @DisplayName("Test to get the last user from a list of users")
+    public void testGetLastUserFromList() {
+        Challenge challenge = new Challenge();
+        List<JSONObject> userList = null;
+        String lastUser = "";
+        try {
+            userList  = challenge.getSortedUsersFromPageThree();
+            lastUser = challenge.getLastUserFromList(userList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = "https://gorest.co.in/public/v2/users?access-token=ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e&page=3";
+        List<JSONObject> test = getUsersFromUrl(url);
+        String lastUserTest = test.get(test.size()-1).get("name").toString();
+
+        Assert.assertEquals(lastUser, lastUserTest);
+    }
+
+    @Test
+    @DisplayName("Test to update the last user name and see if the names are matching accordingly with mock get call")
+    public void testUpdateLastUserNameAndSave() {
+        Challenge challenge = new Challenge();
+        List<JSONObject> userList = null;
+        JSONObject changedUserName = null;
+        String lastUser = "";
+        try {
+            userList  = challenge.getSortedUsersFromPageThree();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = "https://gorest.co.in/public/v2/users?access-token=ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e&page=3";
+        List<JSONObject> test = getUsersFromUrl(url);
+
+        JSONObject lastObject = test.get(test.size()-1);
+        lastObject.put("name", "Peter Parker");
+        changedUserName = challenge.updateLastUserNameAndSave(userList);
+
+        Assert.assertEquals(changedUserName, lastObject);
+
+        // check with a get call as well
+        String changedId = changedUserName.get("id").toString();
+        String changedName = getUserNameById(changedId);
+        Assert.assertEquals(changedUserName.get("name").toString(), changedName);
+    }
+
+    @Test
+    @DisplayName("Deletes a user and then mocks a get call to see if user is deleted - return 404")
+    public void testDeleteUserByUserObject() {
+        Challenge challenge = new Challenge();
+        List<JSONObject> userList = null;
+        JSONObject changedUserName = null;
+        String idToCheck = "";
+        try {
+            userList  = challenge.getSortedUsersFromPageThree();
+            changedUserName = challenge.updateLastUserNameAndSave(userList);
+            idToCheck = changedUserName.get("id").toString();
+            challenge.deleteUserByUserObject(changedUserName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = "https://gorest.co.in/public/v2/users/" + idToCheck +
+                "?access-token=ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e";
+
+
+        int response = getResponseCodeForGetUserById(url);
+        Assert.assertEquals(response, 404);
+    }
+
+    @Test
+    @DisplayName("Test for the deleted user and check if it returns 404 status code")
+    public void testGetUserById() {
+        Challenge challenge = new Challenge();
+        int responseCode = challenge.getResponseCodeById("5555");
+        Assert.assertEquals(responseCode, 404);
+    }
+
+    // Private function to mock get user from url call for sorted user testing
+    private List<JSONObject> getUsersFromUrl(String url) {
+        List<JSONObject> userList = new ArrayList<>();
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .method("GET", null)
+                    .addHeader("X-Pagination-Pages", "")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+            JSONParser parser = new JSONParser();
+
+            JSONArray users = (JSONArray) parser.parse(data);
+            System.out.println(response.headers().get("x-pagination-pages").toString());
+
+            for(int i = 0; i < users.size(); i++) {
+                JSONObject user = (JSONObject) users.get(i);
+                userList.add(user);
+            }
+
+            Collections.sort(userList, new Comparator<JSONObject>() {
+                @Override
+                public int compare(JSONObject o1, JSONObject o2) {
+                    return o1.get("name").toString().compareTo(o2.get("name").toString());
+                }
+            });
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return userList;
+    }
+
+    // private function for getting username by id for checking if username changed
+    private String getUserNameById(String changedId) {
+
+        String url = "https://gorest.co.in/public/v2/users/" + changedId +
+                "?access-token=ec55b886010d2a10dee93bcf93fe2e3efd315c9dc048c4db0ba6bf0cc2232b5e";
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .method("GET", null)
+                    .addHeader("X-Pagination-Pages", "")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+            System.out.println(data);
+            JSONParser parser = new JSONParser();
+            JSONObject user = (JSONObject) parser.parse(data);
+            return user.get("name").toString();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "";
+    }
+
+    // private function to get response status from a mock get call
+    private int getResponseCodeForGetUserById(String url) {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .method("GET", null)
+                    .addHeader("X-Pagination-Pages", "")
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.code();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
